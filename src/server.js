6e50712c8ce2f812bb4175edbd609ba770fabbd8
deivison resende <deivison.resende@enacom.com.br>
2022-03-12 18:37:20 -0300

    Added client to consume data

--- src/server.js
+++ src/server.js
@@ -1,8 +1,33 @@
 import http from 'http';
-const PORT = 3000
+import { Readable } from 'stream';
+import { randomUUID } from 'crypto';
+const PORT = 3000;
 
-async function handler(req, res) {}
+function* run() {
+  for (let index = 0; index < 100; index++) {
+    const data = {
+      id: randomUUID(),
+      name: `Deivison-${index}`
+    };
+    yield data;
+  }
+}
 
-http.createServer(handler)
-.listen(PORT)
-.on('listening', () => console.log(`Server running on port ${PORT}`))
+async function handler(request, response) {
+  const readable = new Readable({
+    read() {
+      for (const data of run()) {
+        console.log(`Sending data`, data);
+        this.push(JSON.stringify(data) + '\n');
+      }
+      this.push(null);
+    }
+  });
+
+  readable.pipe(response);
+}
+
+http
+  .createServer(handler)
+  .listen(PORT)
+  .on('listening', () => console.log(`Server running on port ${PORT}`));
